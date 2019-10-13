34b2e4b6b2a04dafecb9d1f893e29b9065d351e2
niicaps <56492346+niicaps@users.noreply.github.com>
2019-10-12 21:40:24 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,3 +1,27 @@
+
 const Discord = require('discord.js');
 
 const client = new Discord.Client();
+const PREFIX = '.';
+var servers = {};
+var version = '0.0.1'
+const usedCommandRecently = new Set();
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+client.on('message', async message => {
+
+
+
+        let args = message.content.substring(PREFIX.length).split(" ");
+
+
+
+        switch (args[0]) {
+            case 'ping':
+        message.channel.send(randomAnswerPing)
+        break;
+    }
+
+});
+client.login(process.env.BOT_TOKEN)
